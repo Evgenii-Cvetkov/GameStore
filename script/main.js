5c4224d28c54b915faaf7ef78535b53c1f5c7d38
JohnyUk <johny360@mail.ru>
2020-11-17 14:36:41 +0300

    added cycle for games

--- script/main.js
+++ script/main.js
@@ -6,6 +6,8 @@ let header = document.getElementById("header"),
     gamesItemTitle = container.getElementsByClassName('.games__item-title'),
     gameDescr = document.querySelector(".gameDescr"),
     firstGameImage.src = firstGame.cover;
+    showDescr = document.querySelector('.games__item-link_main');
+    otherGames = document.querySelector('.other__games')
 
 
 header.insertAdjacentHTML("afterbegin", "<div class=container><h1 class=main__title>Jeka game store</h1></div>");
@@ -16,39 +18,34 @@ function mainGameTitle() {
   let mainTitle = container.querySelector(".games__item-title");
   mainTitle.innerHTML = games[0].title;
 };
+
 function mainGamePrice() {
   let mainPrice = document.querySelector(".games__item-price");
   mainPrice.innerHTML = games[0].price;
 };
+
 function footer() {
   let footer = document.querySelector(".footer__text");
   footer.innerHTML = "(c)jeka delal etot store";
-}
+};
+
 function mainGameImage() {
   let mainImage = document.querySelector(".games__item-image_main");
   mainImage.innerHTML = games[0].cover;
- }
-const showDescr = document.querySelector('.games__item-link_main');
-const otherGames = document.querySelector('.other__games')
+ };
+
+
 showDescr.addEventListener('click', () => {
   otherGames.classList.toggle('other__games_hide')
 })
 
-// let showDescription = function() {
-//   if (otherGames.classList.contains('other__games_hide')) {
-//     container.insertAdjacentHTML("beforeend", <div class="game__description">dasfsdfasdfa</div>)
-//   }
-
-// }
+let out ='';
+for (i = 1; i < 5; i++) {
+  out += '<img class="other__game-image" src="' + games[i].cover + '">'
+}
+otherGames.innerHTML = out;
 
 mainGameTitle()
 mainGamePrice()
 mainGameImage()
 footer()
-
-// console.log(games[0].cover)
-
-// console.log(games);
-// console.log(games[0]);
-// console.log(images);
-// console.log(firstGameImage);
